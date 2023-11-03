f16bf8397f4a1d437446295e29f46029e93b5ac4
Dmitry Kaigorodov <dima.kaigorodov@gmail.com>
2023-11-03 22:04:54 +0100

    chekFile1.spec.ts

--- tests/chekFile1.spec.ts
+++ tests/chekFile1.spec.ts
@@ -1,40 +1,42 @@
-import { test, expect } from "@playwright/test";
+import { expect, test } from "@playwright/test";
 
 test("Create/Register User Account via API", async ({ page }) => {
-    const apiURL = "https://automationexercise.com/api/createAccount";
-    const userData = {
-      name: "Paw",
-      email: "eEEEee@example.com",
-      password: "P123",
-      title: "Mr",
-      birth_date: "01",
-      birth_month: "January",
-      birth_year: "1999",
-      firstname: "Paw",
-      lastname: "Rud",
-      company: "Company",
-      address1: "54/58 Street St",
-      address2: "Apt 30",
-      country: "Poland",
-      zipcode: "12345",
-      state: "Mazovian",
-      city: "Warsaw",
-      mobile_number: "1234567890",
-    };
-  
-    const response = await page.request.post(apiURL, {
-      data: userData,
-    });
-  
-    const body = await response.json();
+  const postfix = Math.round(Math.random() * 10000)
+  const apiURL = "https://automationexercise.com/api/createAccount";
+  const userData = {
+    name: "Dmitry",
+    email: `eee${postfix}@example.com`,
+    password: "P123",
+    title: "Mr",
+    birth_date: "01",
+    birth_month: "January",
+    birth_year: "1999",
+    firstname: "Paw",
+    lastname: "Rud",
+    company: "Company",
+    address1: "54/58 Street St",
+    address2: "Apt 30",
+    country: "Poland",
+    zipcode: "12345",
+    state: "Mazovian",
+    city: "Warsaw",
+    mobile_number: "123" + postfix,
+  };
 
-    console.log(body);
-  
-    expect(body.responseCode).toBe(200);
-    expect(body.message).toContain("Account created!");
+  const response = await page.request.post(apiURL, {
+    form: userData,
+
   });
-  
-  test("Verify Login with Valid Details via API", async ({ page }) => {
+
+  const body = await response.json();
+
+  console.log(body);
+
+  expect(body.responseCode).toBeLessThan(400);
+  expect(body.message).toContain("User created!");
+});
+
+test("Verify Login with Valid Details via API", async ({ page }) => {
   const apiURL = "https://automationexercise.com/api/verifyLogin";
   const loginData = {
     email: "eee@mail.com",
@@ -56,55 +58,54 @@ test("Create/Register User Account via API", async ({ page }) => {
 });
 
 test("Verify Login with Invalid Details via API", async ({ page }) => {
-    const apiURL = "https://automationexercise.com/api/verifyLogin";
-    const invalidLoginData = {
-      email: "invalid_email@example.com",
-      password: "invalidPassword", 
+  const apiURL = "https://automationexercise.com/api/verifyLogin";
+  const invalidLoginData = {
+    email: "invalid_email@example.com",
+    password: "invalidPassword",
+  };
+
+  const response = await page.request.post(apiURL, {
+    headers: { "Content-Type": "application/json" },
+    data: JSON.stringify(invalidLoginData),
+  });
+
+  const statusCode = response.status();
+  const responseBody = await response.text();
+
+
+  expect(statusCode).toBe(404);
+
+  expect(responseBody).toContain("Your email or password is incorrect!");
+
+});
+
+test("Search Product via API", async ({ page }) => {
+  const apiURL = "https://automationexercise.com/api/searchProduct";
+  const searchTerms = ["Blue", "Yellow"];
+
+  for (const searchTerm of searchTerms) {
+    const searchData = {
+      search_product: searchTerm,
     };
-  
+
     const response = await page.request.post(apiURL, {
       headers: { "Content-Type": "application/json" },
-      data: JSON.stringify(invalidLoginData),
+      data: JSON.stringify(searchData),
     });
-  
-    const statusCode = response.status();
-    const responseBody = await response.text();
-  
-    
-    expect(statusCode).toBe(404);
-    
-    expect(responseBody).toContain("Your email or password is incorrect!");
-  
-  });
-  
-  test("Search Product via API", async ({ page }) => {
-    const apiURL = "https://automationexercise.com/api/searchProduct";
-    const searchTerms = ["Blue", "Yellow"];
-  
-    for (const searchTerm of searchTerms) {
-      const searchData = {
-        search_product: searchTerm,
-      };
-  
-      const response = await page.request.post(apiURL, {
-        headers: { "Content-Type": "application/json" },
-        data: JSON.stringify(searchData),
-      });
-  
-      const statusCode = response.status();
-      const responseBody = await response.json();
-  
-      
 
-      expect(statusCode).toBe(200);
-  
-      if (searchTerm === "Blue") {
-        const blueProducts = responseBody["Searched products list"];
-        expect(blueProducts).toContain("Blue Product 1");
-        expect(blueProducts).toContain("Blue Product 2");
-      } else if (searchTerm === "Yellow") {
-        expect(responseBody).toHaveProperty("error");
-      }
+    const statusCode = response.status();
+    const responseBody = await response.json();
+
+
+
+    expect(statusCode).toBe(200);
+
+    if (searchTerm === "Blue") {
+      const blueProducts = responseBody["Searched products list"];
+      expect(blueProducts).toContain("Blue Product 1");
+      expect(blueProducts).toContain("Blue Product 2");
+    } else if (searchTerm === "Yellow") {
+      expect(responseBody).toHaveProperty("error");
     }
-  });
-  
+  }
+});
